4e11b90ec057217ba8e6fb943173ac09d4f3626d
Sneha M Antonious <snea913@gmail.com>
2024-04-13 11:59:15 -0400

    Test if stack is empty

---
+++ src/test/java/org/example/MyStackTest.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class MyStackTest
+    extends TestCase
+{
+    public MyStackTest(String testName )
+    {
+        super( testName );
+    }
+
+    public void testMyStack()
+    {
+       MyStack<Integer> stack = new MyStack<>();
+        stack.push(1);
+        stack.push(2);
+
+        assertEquals(false, stack.isEmpty());
+
+    }
+}
